2f58c2264c45762272d05f5dad033009e4dce44f
Francesco Anselmo <francesco.anselmo@gmail.com>
2024-12-16 00:07:47 +0000

    working accelerometer example

---
+++ 05_accelerometer_circle/serve.py
@@ -0,0 +1,17 @@
+import http.server
+import socketserver
+import ssl
+
+PORT = 4443  # Choose a port for HTTPS
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+# Create an SSL context
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain('cert.pem', 'key.pem')  # Replace with your certificate and key files
+
+# Create an HTTPS server with the SSL context
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+    print(f"Serving at https://localhost:{PORT}")
+    httpd.serve_forever()
